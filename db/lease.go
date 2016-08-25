07736483f26ef14378d592dc00767e133571ede2
Maria Shaldibina <mshaldibina@pivotal.io>
2016-08-24 17:31:31 -0700

    switch to postgres session locks instead of leasing
    Instead of obtaining lease and keep it heartbeating on interval obtain
    postgres session advisory lock. It will be lost once it is released or
    connection is closed.
    
    [#128029305]
    
    Signed-off-by: Josh Zarrabi <jzarrabi@pivotal.io>

--- db/lease.go
+++ db/lease.go
@@ -1,179 +1,114 @@
 package db
 
 import (
-	"database/sql"
-	"errors"
 	"sync"
-	"time"
 
 	"code.cloudfoundry.org/lager"
+	"github.com/jackc/pgx"
 )
+
+//go:generate counterfeiter . LeaseFactory
+
+type LeaseFactory interface {
+	NewLease(logger lager.Logger, lockID int) Lease
+}
+
+type leaseFactory struct {
+	conn  *pgx.Conn
+	locks lockRepo
+}
+
+func NewLeaseFactory(conn *pgx.Conn) LeaseFactory {
+	return &leaseFactory{
+		conn:  conn,
+		locks: lockRepo{},
+	}
+}
+
+func (f *leaseFactory) NewLease(logger lager.Logger, lockID int) Lease {
+	return &lease{
+		conn:   f.conn,
+		logger: logger,
+		lockID: lockID,
+		locks:  f.locks,
+		mutex:  &sync.Mutex{},
+	}
+}
 
 //go:generate counterfeiter . Lease
 
 type Lease interface {
-	Break()
+	AttemptSign() (bool, error)
+	Break() error
+	AfterBreak(func() error)
 }
 
 type lease struct {
-	conn   Conn
+	conn   *pgx.Conn
 	logger lager.Logger
 
-	attemptSignFunc func(Tx) (sql.Result, error)
-	heartbeatFunc   func(Tx) (sql.Result, error)
-	breakFunc       func()
+	mutex *sync.Mutex // to protect db connection access and locks repo
 
-	medalChan chan struct{}
-	breakChan chan struct{}
-	running   *sync.WaitGroup
+	lockID int
+	locks  lockRepo
+
+	afterBreak func() error
 }
 
-//go:generate counterfeiter . SqlResult
+func (l *lease) AttemptSign() (bool, error) {
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
 
-type SqlResult interface {
-	LastInsertId() (int64, error)
-	RowsAffected() (int64, error)
-}
-
-//go:generate counterfeiter . LeaseTester
-
-type LeaseTester interface {
-	AttemptSign(Tx) (sql.Result, error)
-	Heartbeat(Tx) (sql.Result, error)
-	Break()
-}
-
-func NewLeaseForTesting(conn Conn, logger lager.Logger, leaseTester LeaseTester, interval time.Duration) (Lease, bool, error) {
-	lease := &lease{
-		conn:            conn,
-		logger:          logger,
-		attemptSignFunc: func(tx Tx) (sql.Result, error) { return leaseTester.AttemptSign(tx) },
-		heartbeatFunc:   func(tx Tx) (sql.Result, error) { return leaseTester.Heartbeat(tx) },
-		breakFunc:       func() { leaseTester.Break() },
-	}
-
-	renewed, err := lease.AttemptSign(interval)
-	if err != nil {
-		return nil, false, err
-	}
-
-	if !renewed {
-		return nil, renewed, nil
-	}
-
-	lease.KeepSigned(interval)
-
-	return lease, true, nil
-}
-
-func (l *lease) AttemptSign(interval time.Duration) (bool, error) {
-	tx, err := l.conn.Begin()
-	if err != nil {
-		return false, err
-	}
-
-	defer tx.Rollback()
-
-	result, err := l.attemptSignFunc(tx)
-	if err != nil {
-		return false, err
-	}
-
-	rows, err := result.RowsAffected()
-	if err != nil {
-		return false, err
-	}
-
-	if rows == 0 {
+	if l.locks.IsRegistered(l.lockID) {
 		return false, nil
 	}
 
-	err = tx.Commit()
+	var signed bool
+	err := l.conn.QueryRow(`SELECT pg_try_advisory_lock($1)`, l.lockID).Scan(&signed)
 	if err != nil {
 		return false, err
 	}
 
-	return true, nil
+	l.locks.Register(l.lockID)
+
+	return signed, nil
 }
 
-func (l *lease) KeepSigned(interval time.Duration) {
-	l.medalChan = make(chan struct{}, 1)
-	l.medalChan <- struct{}{}
+func (l *lease) Break() error {
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
 
-	l.breakChan = make(chan struct{})
-
-	l.running = &sync.WaitGroup{}
-	l.running.Add(1)
-
-	go l.keepLeased(interval)
-}
-
-func (l *lease) Break() {
-	if l.medalChan == nil {
-		return
-	}
-
-	select {
-	// the first thread to call break gets a gold medal
-	case <-l.medalChan:
-		close(l.breakChan)
-		l.running.Wait()
-		if l.breakFunc != nil {
-			l.breakFunc()
-		}
-	default:
-	}
-}
-
-func (l *lease) extend() error {
-	tx, err := l.conn.Begin()
+	_, err := l.conn.Exec(`SELECT pg_advisory_unlock($1)`, l.lockID)
 	if err != nil {
 		return err
 	}
 
-	defer tx.Rollback()
+	l.locks.Unregister(l.lockID)
 
-	result, err := l.heartbeatFunc(tx)
-	if err != nil {
-		return err
-	}
-
-	rows, err := result.RowsAffected()
-	if err != nil {
-		return err
-	}
-
-	if rows == 0 {
-		return errors.New("lease not found")
-	}
-
-	err = tx.Commit()
-	if err != nil {
-		return err
+	if l.afterBreak != nil {
+		return l.afterBreak()
 	}
 
 	return nil
 }
 
-func (l *lease) keepLeased(interval time.Duration) {
-	defer l.running.Done()
+func (l *lease) AfterBreak(afterBreakFunc func() error) {
+	l.afterBreak = afterBreakFunc
+}
 
-	ticker := time.NewTicker(interval / 2)
-	defer ticker.Stop()
+type lockRepo map[int]bool
 
-dance:
-	for {
-		select {
-		case <-ticker.C:
-			err := l.extend()
-			if err != nil {
-				l.logger.Error("failed-to-renew-lease", err)
-				break dance
-			}
-
-			l.logger.Debug("renewed-the-lease")
-		case <-l.breakChan:
-			break dance
-		}
+func (lr lockRepo) IsRegistered(lockID int) bool {
+	if _, ok := lr[lockID]; ok {
+		return true
 	}
+	return false
+}
+
+func (lr lockRepo) Register(lockID int) {
+	lr[lockID] = true
+}
+
+func (lr lockRepo) Unregister(lockID int) {
+	delete(lr, lockID)
 }
