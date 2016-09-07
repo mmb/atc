5787332d12cab5b401ae715434795c9eec3e2a18
James Quintana <jquintana@pivotal.io>
2016-09-07 12:32:09 -0700

    wip wip
    Signed-off-by: James Quintana <jquintana@pivotal.io>

--- web/elm/src/Resource.elm
+++ web/elm/src/Resource.elm
@@ -98,9 +98,11 @@ update action model =
     AutoupdateTimerTicked timestamp ->
       ( model
       , Cmd.batch
-          [ fetchResource model.resourceIdentifier
-          , fetchVersionedResources model.resourceIdentifier model.currentPage
-          ]
+          List.concat
+            [ fetchResource model.resourceIdentifier
+            , fetchVersionedResources model.resourceIdentifier model.currentPage
+            ]
+            updateExpandedProperties model
       )
     ResourceFetched (Ok resource) ->
       ( { model | resource = Just resource }
@@ -600,6 +602,28 @@ viewBuildsByJob buildDict jobName =
           (List.map oneBuildToLi buildList)
       )
     ]
+
+updateExpandedProperties : Model -> List (Cmd Msg)
+updateExpandedProperties model =
+  -- we have access to:
+     -- model.versionedResources      List (Concourse.VersionedResource)
+        -- versionedResource.id       Int -----.
+     -- model.versionedUIStates       Dict of Int to VersionedUIState
+        -- VersionedUIState.expanded  Bool
+  --want to create a Cmd that:
+      -- fetches inputs and outputs for every expanded versionedResource
+  --methods you might use:
+    -- List.map or maybe List.concatMap
+    -- List.filter
+    -- you will probably have to define a few "helper functions" either inline like we did before or like you see below
+let
+
+in
+
+-- we can add a parameter to this method, and "lock it in" with a value later
+-- hint: the new parameter MUST be the first parameter
+-- if you're stuck, consider reading about "currying" functions
+ourCondition : Concourse.VersionedResource -> Bool
 
 autoupdateTimer : Model -> Sub Msg
 autoupdateTimer model =
